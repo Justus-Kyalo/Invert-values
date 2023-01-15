f1d4d740e6370fdc155c764769e7bec9ca425e09
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-01-15 15:43:29 +0300

    solved

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function invert(list) {
-    //Using the map method to iterate over the array
+    //Using the map method to iterate over the array list
     //and applying the negation operation on each element
     return list.map(x => -x);
 }
